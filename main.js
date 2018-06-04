2c213711d50efdc7f048e52288bb935498d37603
Travis Anderson <thetanderson@gmail.com>
2018-06-04 15:52:11 -0400

    update grid

--- main.js
+++ main.js
@@ -1,54 +1,94 @@
-function Grid(height, width, name) {
+function Grid(height, width, name, gridElement) {
     this.height = height;
     this.width = width;
     this.name = name;
+    this.element = gridElement
     this.fillGrid()
-    this.findCell(1, 2)
-    Cell.call(this)
     
     
 
-}
+
+this.neighbors = [
+    {x: x=0, 
+    y: y=1},
+    {x: x=0, 
+    y: y=-1},
+    {x: x=1, 
+    y: y=0},
+    {x: x=1, 
+    y: y=-1},
+    {x: x=1, 
+    y: y=1},
+    {x: x=-1,
+    y: y=0},
+    {x: x=-1,
+    y: y=1},
+    {x: x=-1,
+    y: y=-1}
+            ]
+        }
 
 Grid.prototype = {
+    
     fillGrid: function () {
-        let main = document.getElementById("main")
+
+        this.element.addEventListener('click', this.clickHandler.bind(this))
         
         this.sampleArray = new Array(this.height).fill().map(cell => new Array(this.width).fill(0))
-        this.sampleArray.elementProperty = "hello"
+        this.sampleArray.elementProperty = "elementProperty"
         this.sampleArray.forEach((_, rowIndex) => {
-            let rowDiv = document.createElement("section")
-            // this.sampleArray[rowIndex] = 'big'
-            rowDiv.dataset.row = rowIndex
-            main.appendChild(rowDiv)
+            let rowElement = document.createElement("section")
+            rowElement.dataset.row = rowIndex
+            this.element.appendChild(rowElement)
             _.forEach((_, colIndex) => {
-                // this.sampleArray[rowIndex][colIndex] = new Cell(rowIndex, colIndex, rowDiv).createCell()
-                // cellElement.createCell()
-                this.sampleArray[rowIndex][colIndex] = new Cell(rowIndex, colIndex, rowDiv)
-                
+                this.sampleArray[rowIndex][colIndex] = new Cell(rowIndex, colIndex, rowElement, this)
             });
         })
         console.table(this.sampleArray)
     },
 
-    
-    findCell: function (row, column) {
-        console.log(this.sampleArray[row][column])  
-        return this.sampleArray[row][column]
+    clickHandler: function(event) {
+        const clickedCell = this.findCell(event.target.dataset.row, event.target.dataset.col)
+        this.findNeighbors(clickedCell)
     },
 
-    findNeighbors: function () {
-        // let newNeighbor = new Cell()
+    findCell: function (rowIndex, columnIndex) {
+        const row = this.sampleArray[parseInt(rowIndex)]
+        // if (row) {
+        //     return row[parseInt(columnIndex)]
+        // } else {
+        //     return null
+        // }
+        return row && row[parseInt(columnIndex)]
     },
+
+    findNeighbors: function(clickedCell) {
+        let neighborsArray = []
+        
+        console.log(' i click on this', clickedCell) 
+        for(let i = 0; i<this.neighbors.length; i++){
+            let xNeighbor = clickedCell.rowIndex + this.neighbors[i].x
+            let yNeighbor = clickedCell.colIndex + this.neighbors[i].y
+            let neighborCell = this.findCell(xNeighbor, yNeighbor)
+
+            if (neighborCell){
+                neighborsArray.push(neighborCell)
+            }
+    }
+    console.log(neighborsArray)
+},
 }
 
-function Cell(rowIndex, colIndex, parent) {
+
+
+
+
+function Cell(rowIndex, colIndex, parent, grid) {
     this.rowIndex = rowIndex;
     this.colIndex = colIndex;
     this.parent = parent;
-    this.createCell();
-    
-    
+    this.grid = grid;
+    this.createCell();  
 }
 
 Cell.prototype.createCell = function () {
@@ -56,23 +96,14 @@ Cell.prototype.createCell = function () {
     this.element.dataset.row = this.rowIndex;
     this.element.dataset.col = this.colIndex;
     this.parent.appendChild(this.element);
-    this.element.addEventListener('click', this.getCell.bind(this))
 }
 
 Cell.prototype.getCell = function () {
     
-    console.log(this.element)
     return this.element
-
-}
-Cell.prototype.hiMom = function () {
     
-    console.log('hi mom')
-    
-
 }
 
-
-let bob = new Grid(3, 5, "name")
+let bob = new Grid(8, 8, "name",document.getElementById("main"))
 
 
